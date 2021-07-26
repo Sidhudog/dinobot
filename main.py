d0ae748c59cfc72b9d1ae618d1b31fed703c79d5
Sudhanshu Kumar <64831792+Sidhudog@users.noreply.github.com>
2021-07-26 17:40:59 +0530

    Added main  program

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+if __name__ == "__main__":
+    print("Dino game about to start")
+    time.sleep(3)
+    hit('up')
+    cnt=0
+    speed1 = 420
+    speed2 = 400
+    ac=speed1
+    while True:
+        speed1 = speed1 + ac*0.00032
+        speed2 = speed2 + ac*0.00032
+        image = takess()
+        data = image.load()
+            # if ismidbird(data):
+            #     hit("down")
+        print(str(speed1)+"\n")
+
+        if iscolourchange(data,cnt):
+            if isCollidedark(data,speed1,speed2):
+                hit("up")
+        else:
+            if isCollide(data,speed1,speed2):
+                hit("up")
+        
+
+    # print(asarray(data))
+    draw(data,speed1)
+    image.show()
 
